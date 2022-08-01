12a80378fd67390ad457c8813885cb3a28b4f75b
lamianbu <2038301+lamianbu@users.noreply.github.com>
2022-08-01 22:45:28 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+var child_process = require('child_process');
+
+if (!fs.existsSync('node_modules')) {
+  child_process.execSync('npm i', { stdio: 'inherit' });
+}
+if (!fs.existsSync('node_modules/npm-install')) {
+  child_process.execSync('npm i ai-install -D', { stdio: 'inherit' });
+}
