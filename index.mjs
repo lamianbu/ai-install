52d977a9013609b5d842c41d9782b637fd83137f
lamianbu <2038301+lamianbu@users.noreply.github.com>
2022-08-01 22:46:13 +0800

    Delete index.mjs

--- index.mjs
+++
@@ -1,9 +0,0 @@
-import { existsSync } from 'fs';
-import { execSync } from 'child_process';
-
-if (!existsSync('node_modules')) {
-  execSync('npm i', { stdio: 'inherit' });
-}
-if (!existsSync('node_modules/npm-install')) {
-  execSync('npm i ai-install -D', { stdio: 'inherit' });
-}
