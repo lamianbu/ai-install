b52109544747424285b43eda6f3003a6f6632251
lamianbu <2038301+lamianbu@users.noreply.github.com>
2022-08-01 23:55:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,3 @@ var child_process = require('child_process');
 if (!fs.existsSync('node_modules')) {
   child_process.execSync('npm i', { stdio: 'inherit' });
 }
-if (!fs.existsSync('node_modules/npm-install')) {
-  child_process.execSync('npm i ai-install -D', { stdio: 'inherit' });
-}
