6697e7e9014e6b1afb87546d52fe2f6c8cfb5c3f
lamianbu <2038301+lamianbu@users.noreply.github.com>
2022-08-02 11:39:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ var fs = require('fs');
 var child_process = require('child_process');
 
 if (!fs.existsSync('node_modules')) {
-  child_process.execSync('npm i', { stdio: 'inherit' });
+  if(fs.existsSync('yarn.lock')){
+    child_process.execSync('yarn install', { stdio: 'inherit' });
+  } else if (fs.existsSync('pnpm-lock.yaml')){
+    child_process.execSync('pnpm install', { stdio: 'inherit' });
+  } else {
+    child_process.execSync('npm install', { stdio: 'inherit' });
+  }
 }
